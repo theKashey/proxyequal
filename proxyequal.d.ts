1d13b981f8b4813413a7ed3eed3276a1be8a28b8
Anton Korzunov <thekashey@gmail.com>
2018-02-19 17:12:52 +1100

    CI

--- proxyequal.d.ts
+++ proxyequal.d.ts
@@ -1,14 +1,21 @@
+import {ProxyStateResult} from "proxyequal";
+
 declare module 'proxyequal' {
+
+    interface ProxyStateResult<T> {
+        state: T,
+        affected: string[],
+        reset: () => void,
+
+        replaceState(state: T): ProxyStateResult<T>
+    };
 
     /**
      * Wraps state with traps
      * @param {T} state - the "state" object
      * @return {{state: T; affected: string[]}}
      */
-    export function proxyState<T>(state: T) : {
-        state: T,
-        affected: string[]
-    }
+    export function proxyState<T>(state: T): ProxyStateResult<T>;
 
     /**
      * Test is left equal to the right on variable-value level
